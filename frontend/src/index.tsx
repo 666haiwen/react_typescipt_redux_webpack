efdb31b206d5306c2ce13b395ef8cf36c5e65318
Chenzexian <zjuclce@gmail.com>
2019-11-07 17:00:17 +0800

    remove console

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import configureStore from './configureStore';
 
 const store = configureStore();
-console.log(store.getState());
 render(
   <Root store={store} />,
   document.getElementById('root')
